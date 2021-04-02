2fdb2692c294f425d881843a3b96ef26054d24f1
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-04-02 09:04:25 +0200

    chore: Blocks subgraph (#27)

--- utils/blocks/client.ts
+++ utils/blocks/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const blockClient = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "https://api.bscgraph.org/subgraphs/name/bsc-blocks",
+    uri: "https://api.thegraph.com/subgraphs/name/pancakeswap/blocks",
   }),
   cache: new InMemoryCache(),
 });
