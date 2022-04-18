697cdb63288e60166a2e8fe1a879d1ea66888449
erwin-huang <erwinhuank@gmail.com>
2022-04-18 16:56:25 +0700

    fix: Update link and readme

--- utils/blocks/client.ts
+++ utils/blocks/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const blockClient = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "https://api.thegraph.com/subgraphs/name/pancakeswap/blocks",
+    uri: "https://api.thegraph.com/subgraphs/name/koda-finance/blocks",
   }),
   cache: new InMemoryCache(),
 });
