17bd13fd931938c708ae0903284fe27f2461d379
erwin-huang <erwinhuank@gmail.com>
2022-07-04 15:21:11 +0700

    feat: Update summitswap-exchange api

--- utils/apollo/client.ts
+++ utils/apollo/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const client = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "https://api.thegraph.com/subgraphs/name/koda-finance/summitswap-exchange",
+    uri: "https://api.thegraph.com/subgraphs/id/QmQFajWj8nsFoe6T6EKhDE6WiemaM5f61VzPiDdxbo6Lwb",
   }),
   cache: new InMemoryCache(),
 });
